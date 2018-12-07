39461670c26126449ec4f404af922f9470174885
Brooke Billmaier <brooke@Brooke.local>
2018-12-07 14:02:59 -0500

    fixing sticky nav

--- js/index.js
+++ js/index.js
@@ -68,14 +68,6 @@ $("#resizable").resizable({
 
 // nav bar trigger
 
-$('.button-collapse').sideNav({
-    menuWidth: 300, // Default is 300
-    edge: 'left', // Choose the horizontal origin
-    closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    draggable: true // Choose whether you can drag to open on touch screens,
-  }
-); 
-
 
   // Sticky Navbar Functionality
   $(document).ready(function(){
